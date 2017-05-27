ad424be06fd2e5fd7c126ce7745e7bf297af3ca1
jiangkunfeng <1209096864@qq.com>
2017-05-27 13:37:09 +0800

    34

--- src/com/test/Demo4.java
+++ src/com/test/Demo4.java
@@ -4,13 +4,13 @@ package com.test;
  * Created by jiang on 26.
  */
 public class Demo4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Demo2");
 
         System.out.println("master--1");
 
         System.out.println("master--2");
 
-        System.out.println("master--3");
+        System.out.println("master--3-2");
     }
 }
