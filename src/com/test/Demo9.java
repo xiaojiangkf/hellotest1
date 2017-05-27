a9d4818a64f99eb4b68fee7519f9486c1a6ddc6a
jiangkunfeng <1209096864@qq.com>
2017-05-27 14:31:13 +0800

    a1

--- src/com/test/Demo9.java
+++ src/com/test/Demo9.java
@@ -7,5 +7,6 @@ public class Demo9 {
     public static void main(String[] args){
         System.out.println("Demo06");
         System.out.println("Demo06-1");
+        System.out.println("Demo06-1");
     }
 }
