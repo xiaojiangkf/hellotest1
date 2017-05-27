a5cacb1592e68ea6ab60ec6103ca426f97617073
jiangkunfeng <1209096864@qq.com>
2017-05-27 14:30:30 +0800

    a1

---
+++ src/com/test/Demo9.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by jiang on 27.
+ */
+public class Demo9 {
+    public static void main(String[] args){
+        System.out.println("Demo06");
+        System.out.println("Demo06-1");
+    }
+}
