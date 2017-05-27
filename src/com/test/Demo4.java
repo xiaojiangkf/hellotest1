abfcdf21b35cde091cc1622968a9d458ca4a47bd
jiangkunfeng <1209096864@qq.com>
2017-05-27 11:17:59 +0800

    3

--- src/com/test/Demo4.java
+++ src/com/test/Demo4.java
@@ -6,7 +6,5 @@ package com.test;
 public class Demo4 {
     public static void main(String[] args){
         System.out.println("Demo2");
-        
-        System.out.println("master-2");
     }
 }
