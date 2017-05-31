ce61632fd67e1f64faa0eb5d9bfef46a89f9764c
jiangkunfeng <1209096864@qq.com>
2017-05-31 09:58:26 +0800

    add a file Demo8.java

---
+++ src/com/test/Demo8.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * Created by jiang on 27.
+ */
+public class Demo8 {
+    public static void main(String[] args){
+        System.out.println("Demo06");
+        System.out.println("Demo06-1");
+    }
+}
